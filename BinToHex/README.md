864c31a490143b676dc191f372829e58b1d9a011
doyler <dev@doyler.net>
2015-07-17 14:16:51 +0200

    BinToHex commit

---
+++ BinToHex/README.md
@@ -0,0 +1,4 @@
+BinToHex
+===============
+
+A simple python script for converting binary files to encoded hex written in Python
---
+++ BinToHex/binToHex.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+#!/usr/bin/env python
+if __name__ == "__main__":
+ 
+	shellcode = "\""
+	ctr = 1
+	maxlen = 15
+ 
+	for b in open("win-exec-calc-shellcode.bin", "rb").read():
+		shellcode += "\\x" + b.encode("hex")
+		if ctr == maxlen:
+			shellcode += "\" +\n\""
+			ctr = 0
+		ctr += 1
+	shellcode += "\""
+	print shellcode
